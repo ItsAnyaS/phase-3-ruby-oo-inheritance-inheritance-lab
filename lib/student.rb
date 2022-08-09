c90a09339a4100623e7f31a7bf9aee6277b2eb3c
anastasia <anyasirman@outlook.com>
2022-08-09 17:17:16 -0400

    commit tree e64b854b01bdbb846c828240b3895f43a42d6df0

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
 
+class Student < User
+
+    attr_accessor :initalize, :learn, :knowledge
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(fact)
+      @knowledge << fact
+    end
 end
